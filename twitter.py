9a5a3e9e692a7f8815731492695cc0d63e6d368f
vincenthang <gghvince@gmail.com>
2018-05-12 14:40:49 -0400

    integrated flask api for twitter searches

--- twitter.py
+++ twitter.py
@@ -1,4 +1,42 @@
 from twitterscraper import query_tweets
+from flask import Flask
+from flask import jsonify
+
+app = Flask(__name__)
+
+def getSearchJSON(keyword, multiplier):
+	list_of_tweets = query_tweets(keyword, multiplier)
+	a = []
+	for tweet in list_of_tweets:
+		e = {}
+		e['user'] 		= tweet.user
+		e['fullname'] 	= tweet.fullname
+		e['id'] 		= tweet.id
+		e['url'] 		= tweet.url
+		e['timestamp'] 	= tweet.timestamp
+		e['replies'] 	= tweet.replies
+		e['likes'] 		= tweet.likes
+		e['html'] 		= tweet.html
+		a.append(e)
+	d = {'tweets': a }
+	return d
+
+@app.route('/twitter/<search>')
+def twitterSearch(search):
+	return jsonify(getSearchJSON(str(search), 5))
+
+
+if __name__ == "__main__":
+	searches = ["Ethereum from:VitalikButerin", "Ethereum"]
+	print(getSearchJSON(searches[0],20))
+
+	app.run(threaded=True)
+
+''' USEFUL DEBUG FUNCTIONS
+def printDateTest(keyword, multiplier):
+	for tweet in query_tweets(keyword, multiplier):
+		print("timestamp: ", tweet.timestamp)
+
 
 def printSimpleTest(keyword, multiplier):
 	list_of_tweets = query_tweets(keyword, multiplier)
@@ -13,12 +51,4 @@ def printSimpleTest(keyword, multiplier):
 		print("retweets: ", tweet.retweets)
 		print("likes: ", tweet.likes)
 		print("html: ", tweet.html)
-
-def printDateTest(keyword, multiplier):
-	for tweet in query_tweets(keyword, multiplier):
-		print("timestamp: ", tweet.timestamp)
-
-if __name__ == "__main__":
-	searches = ["Ethereum from:VitalikButerin", "Ethereum"]
-	printDateTest(searches[1], 100)
-
+'''
